36d5bfd8582ff89e28b58fbb918e0b47078b8069
Yeongmin_Chae <codudals98@naver.com>
2024-03-26 16:23:41 +0900

    tailwind css 값을 커스텀 하는 방식들

--- app/page.tsx
+++ app/page.tsx
@@ -2,17 +2,9 @@ export default function Home() {
   return (
     <main className="flex h-screen items-center justify-center bg-slate-300 p-5 sm:bg-red-100 md:bg-green-100 lg:bg-cyan-100 xl:bg-orange-100 2xl:bg-purple-100 dark:bg-slate-600">
       <div className="flex w-full max-w-screen-sm flex-col gap-4 rounded-3xl bg-white p-5 shadow-lg">
-        <div className="group flex flex-col">
-          <input
-            type="email"
-            className="w-full bg-gray-100"
-            placeholder="Write your email peer"
-          />
-          <span className="hidden group-focus-within:block">
-            Make sure it is a valid email.
-          </span>
-          <button>Submit</button>
-        </div>
+        <button className="rounded-super bg-black text-[3rem] text-white ">
+          Submit
+        </button>
       </div>
     </main>
   );
