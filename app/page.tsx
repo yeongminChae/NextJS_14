9cb6cc04c777e1c73a55a2ee6d34b654d6c4fdba
Yeongmin_Chae <codudals98@naver.com>
2024-03-26 17:10:40 +0900

    tailwind css : base, components, utilities

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,7 @@ export default function Home() {
   return (
     <main className="flex h-screen items-center justify-center bg-slate-300 p-5 sm:bg-red-100 md:bg-green-100 lg:bg-cyan-100 xl:bg-orange-100 2xl:bg-purple-100 dark:bg-slate-600">
       <div className="flex w-full max-w-screen-sm flex-col gap-4 rounded-3xl bg-white p-5 shadow-lg">
-        <button className="rounded-super bg-black text-[3rem] text-white ">
-          Submit
-        </button>
+        <button className="btn">Submit</button>
       </div>
     </main>
   );
