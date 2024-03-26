4291d31c8a231221cc18619733743a03fab1851d
Yeongmin_Chae <codudals98@naver.com>
2024-03-26 15:18:22 +0900

    tailwind css state modifier *: (모든 자식 요소에 적용), has-[.peer] peer클래스 여부 확인, has[:invalid] invalid한 요소 있는지 확인

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,17 @@
 export default function Home() {
   return (
     <main className="flex h-screen items-center justify-center bg-slate-300 p-5 sm:bg-red-100 md:bg-green-100 lg:bg-cyan-100 xl:bg-orange-100 2xl:bg-purple-100 dark:bg-slate-600">
-      <div className="flex w-full max-w-screen-sm flex-col gap-2 rounded-3xl bg-white p-5 shadow-lg md:flex-row dark:bg-gray-700">
+      <div className="flex w-full max-w-screen-sm flex-col gap-2 rounded-3xl bg-white p-5 shadow-lg ring-transparent transition-shadow *:outline-none has-[.peer]:bg-green-100 has-[:invalid]:ring-red-100 md:flex-row dark:bg-gray-700">
         <input
-          type="email"
+          type="text"
           placeholder="Email address"
           required
-          className="peer h-10 w-full rounded-full bg-gray-200 pl-5 outline-none ring ring-transparent transition-shadow placeholder:drop-shadow focus:shadow-md focus:ring-green-500 focus:ring-offset-2 invalid:focus:ring-red-500"
+          className="peer h-10 w-full rounded-full bg-gray-200 pl-5 ring ring-transparent transition-shadow placeholder:drop-shadow focus:shadow-md focus:ring-green-500 focus:ring-offset-2 invalid:focus:ring-red-500"
         />
         <span className="hidden font-medium text-red-500 peer-invalid:block">
           Email is Required.
         </span>
-        <button className="rounded-full bg-black py-2 font-medium text-white outline-none transition-transform active:scale-90 peer-required:bg-green-500 md:px-10">
+        <button className="rounded-full bg-black py-2 font-medium text-white transition-transform active:scale-90 peer-required:bg-green-500 md:px-10">
           Search
         </button>
       </div>
