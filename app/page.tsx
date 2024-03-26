4c8efe6eb38ba0a5af7909e00916b6b46a8b39f9
Yeongmin_Chae <codudals98@naver.com>
2024-03-26 18:08:43 +0900

    tailwind css의 classname 유틸리트들을 @tailwind components를 사용해서 개별 파일로 분리해서 작성하기

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,10 @@
+import styles from "../styles/style.module.css";
+
 export default function Home() {
   return (
     <main className="flex h-screen items-center justify-center bg-slate-300 p-5 sm:bg-red-100 md:bg-green-100 lg:bg-cyan-100 xl:bg-orange-100 2xl:bg-purple-100 dark:bg-slate-600">
       <div className="flex w-full max-w-screen-sm flex-col gap-4 rounded-3xl bg-white p-5 shadow-lg">
-        <button className="btn">Submit</button>
+        <button className={styles.btn}>Submit</button>
       </div>
     </main>
   );
