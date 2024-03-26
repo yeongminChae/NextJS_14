7113790b0ef23f893ee074e6ca1a591a6f00a2c6
Yeongmin_Chae <codudals98@naver.com>
2024-03-26 12:45:57 +0900

    tailwind css modifiers (hover: , dark: ,) + (max-w-screen-sm , transition )

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
-    <main className="flex h-screen items-center justify-center bg-slate-300 p-5">
-      <div className="w-full rounded-2xl bg-white p-5 shadow-lg">
+    <main className="flex h-screen items-center justify-center bg-slate-300 p-5 dark:bg-slate-600">
+      <div className="w-full max-w-screen-sm rounded-2xl bg-white p-5 shadow-lg dark:bg-gray-700">
         <div className="flex items-center justify-between ">
           <div className="flex flex-col">
             <span className="-mb-1 font-semibold text-gray-600">
@@ -12,7 +12,7 @@ export default function Home() {
           <div className="size-12 rounded-full bg-orange-400"></div>
         </div>
         <div className="my-2 flex items-center gap-2">
-          <span className="rounded-full bg-green-400 px-2.5 py-1.5 text-xs font-medium uppercase text-white">
+          <span className="rounded-full bg-green-400 px-2.5 py-1.5 text-xs font-medium uppercase text-white transition hover:scale-125 hover:bg-green-500">
             Today
           </span>
           <span>9:30 - 10:30</span>
