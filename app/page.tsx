c541bbc7adf02fe004c7f07f761d0925d4f7eaed
Yeongmin_Chae <codudals98@naver.com>
2024-03-26 15:28:46 +0900

    tailwind css : odd, even, first, last

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,19 @@
 export default function Home() {
   return (
     <main className="flex h-screen items-center justify-center bg-slate-300 p-5 sm:bg-red-100 md:bg-green-100 lg:bg-cyan-100 xl:bg-orange-100 2xl:bg-purple-100 dark:bg-slate-600">
-      <div className="flex w-full max-w-screen-sm flex-col gap-2 rounded-3xl bg-white p-5 shadow-lg ring-transparent transition-shadow *:outline-none has-[.peer]:bg-green-100 has-[:invalid]:ring-red-100 md:flex-row dark:bg-gray-700">
-        <input
-          type="text"
-          placeholder="Email address"
-          required
-          className="peer h-10 w-full rounded-full bg-gray-200 pl-5 ring ring-transparent transition-shadow placeholder:drop-shadow focus:shadow-md focus:ring-green-500 focus:ring-offset-2 invalid:focus:ring-red-500"
-        />
-        <span className="hidden font-medium text-red-500 peer-invalid:block">
-          Email is Required.
-        </span>
-        <button className="rounded-full bg-black py-2 font-medium text-white transition-transform active:scale-90 peer-required:bg-green-500 md:px-10">
-          Search
-        </button>
+      <div className="flex w-full max-w-screen-sm flex-col gap-4 rounded-3xl bg-white p-5 shadow-lg dark:bg-gray-700">
+        {["Nico", "Me", "You", "Yourself"].map((person, index) => (
+          <div
+            key={index}
+            className="flex items-center gap-5 rounded-xl border-b-2 border-gray-500 p-2.5 pb-5 first:border-0 last:border-0 odd:bg-gray-100 even:bg-cyan-100"
+          >
+            <div className="size-10 rounded-full bg-blue-400" />
+            <span className="text-lg font-medium ">{person}</span>
+            <div className="flex size-6 items-center justify-center rounded-full bg-red-500 text-white">
+              <span>{index}</span>
+            </div>
+          </div>
+        ))}
       </div>
     </main>
   );
