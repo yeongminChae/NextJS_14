dba41ed14922dd4304d9ada8c8e70b7bbba281a1
Yeongmin_Chae <codudals98@naver.com>
2024-03-28 11:34:25 +0900

    forminput 컴포넌트 -> input 컴포넌트로 이름 수정, InputHTMLAttributes<HTMLInputElement>를 사용해서 input 컴포넌트가 InputHTMLAttributes이 받는 모든 props들에 접근 가능하도록 수정 ...rest props은 모든 props를 한번에 받아올 수 있도록 해주는 키워드

--- components/input.tsx
+++ components/input.tsx
@@ -1,25 +1,20 @@
-interface FormInputProps {
-  type: string;
-  placeholder: string;
-  required: boolean;
+import { InputHTMLAttributes } from "react";
+
+interface InputProps {
   errors?: string[];
   name: string;
 }
 
-export default function FormInput({
-  type,
-  placeholder,
-  required,
-  errors = [],
+export default function Input({
   name,
-}: FormInputProps) {
+  errors = [],
+  ...rest
+}: InputProps & InputHTMLAttributes<HTMLInputElement>) {
   return (
     <div className="flex flex-col gap-2">
       <input
         name={name}
-        type={type}
-        placeholder={placeholder}
-        required={required}
+        {...rest}
         className="h-10 w-full rounded-md border-none bg-transparent ring-2 ring-neutral-200 transition placeholder:text-neutral-400 focus:outline-none focus:ring-4 focus:ring-orange-500"
       />
       {errors.map((error, index) => (
