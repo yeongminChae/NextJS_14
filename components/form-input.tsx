697e32d249e08d442e51c5d11d7df3c46ca405f1
Yeongmin_Chae <codudals98@naver.com>
2024-03-28 10:44:24 +0900

    버그 수정 : map함수를 잘못 사용해서, 에러 메시지 대신, map의 인덱스가 출력되던 버그 해결
    map 함수의 첫번째 인자는 배열의 각 요소의 값이고, 두 번째 인자는 해당 요소의 인덱스이다.
    나는 각 요소의 값과 요소의 인덱스 순서를 잘못 사용함.

--- components/form-input.tsx
+++ components/form-input.tsx
@@ -22,7 +22,7 @@ export default function FormInput({
         required={required}
         className="h-10 w-full rounded-md border-none bg-transparent ring-2 ring-neutral-200 transition placeholder:text-neutral-400 focus:outline-none focus:ring-4 focus:ring-orange-500"
       />
-      {errors.map((index, error) => (
+      {errors.map((error, index) => (
         <span key={index} className="font-medium text-red-500">
           {error}
         </span>
